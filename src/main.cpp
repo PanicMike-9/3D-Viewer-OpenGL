7865759a1e2b3843c53d5ed5071564fa377173b0
PanicMike-9 <omsaudager990@gmail.com>
2026-05-28 18:21:37 +0530

    Update: add vertex array object(VAO)

--- src/main.cpp
+++ src/main.cpp
@@ -63,6 +63,17 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // vertex array object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     std::cout << "GLAD load success!\n";
     
     // custom window color
@@ -74,8 +85,12 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         process_input(window);
+
+        glBindVertexArray(VAO);
         
         glfwSwapBuffers(window);
+        glDrawArrays(GL_TRIANGLES, 0, 3); // draw the vertices and arrays
+
         glfwPollEvents();
     }
 
