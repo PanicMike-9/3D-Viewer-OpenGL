350bed976f08eb761b5e6585bd2a284ab98da334
PanicMike-9 <omsaudager990@gmail.com>
2026-06-01 14:48:00 +0530

    Update(shader.hpp/.cpp): add set_mat4 and glm related headers and define set_mat4 function

--- src/shader.cpp
+++ src/shader.cpp
@@ -116,3 +116,8 @@ void Shader::set_float(const std::string &name, float value) const
 {
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
+
+void Shader::set_mat4(const std::string &name, const glm::mat4 &mat) const
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+}
--- src/shader.hpp
+++ src/shader.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
-#include <glad/glad.h> // all OpenGL headers
+// all OpenGL headers
+#include <glad/glad.h> 
+
+// glm libraries
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <string>
 #include <fstream>
@@ -22,4 +27,7 @@ class Shader
         void set_bool(const std::string &name, bool value) const;
         void set_int(const std::string &name, int value) const;
         void set_float(const std::string &name, float value) const;
+
+        // set matrix 4 for glm
+        void set_mat4(const std::string &name, const glm::mat4 &mat) const;
 };
