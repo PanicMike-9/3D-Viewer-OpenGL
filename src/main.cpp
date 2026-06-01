6501d1159273bc53d25207fc79b990e84cddd184
PanicMike-9 <omsaudager990@gmail.com>
2026-06-01 14:36:41 +0530

    Update(main.cpp): fix vbo, vao and ebo, add model, view and projection logic in main render loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+// include glm for openGL math
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 #include "shader.hpp"
@@ -21,7 +27,7 @@ int main()
   glfwInit();
 
   // declare window name and size
-  GLFWwindow* window = glfwCreateWindow(win_width, win_height, "3d Viewer", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(win_width, win_height, "OpenGL Cube", NULL, NULL);
 
   if(!window)
   {
@@ -55,88 +61,104 @@ int main()
    -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
   };
 
+  float cube_vertices[]
+  {
+    -0.5f, -0.5f, 0.5f,
+     0.5f, -0.5f, 0.5f,
+     0.5f,  0.5f, 0.5f,
+    -0.5f,  0.5f, 0.5f,
+
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+  };
+
+  GLuint cube_indices[]
+  {
+      // front face
+      0, 1, 2,
+      0, 2, 3,
+
+      // back face
+      4, 5, 6,
+      4, 6, 7,
+
+      // right face
+      1, 5, 6,
+      1, 6, 2,
+
+      // left face
+      0, 4, 7,
+      0, 7, 3,
+
+      // top face
+      2, 3, 7,
+      2, 7, 6,
+
+      // bottom face
+      0, 1, 5,
+      0, 5, 4,
+  };
+
   // vertex buffer object (VBO)
   GLuint vbo = 0;
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(tri_vertices), tri_vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
 
   // vertex array object(VAO)
   GLuint vao = 0;
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
-  glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, vao);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
   // position
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
   // color
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // element buffer objec (EBO) for cube
+  GLuint ebo = 0;
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+
+  // unbind
+  glBindVertexArray(0);
 
   // shader code files
   Shader shader("shaders/v_shader.vert", "shaders/f_shader.frag");
 
-  float angle = 0.0f; // angle changes
-
-  // x and y coordinate values
-  float x_offset = 0.0f;
-  float y_offset = 0.0f;
+  // enable depth test
+  glEnable(GL_DEPTH_TEST);
 
   // main window loop
   while(!glfwWindowShouldClose(window))
   {
-    // animation values
-    float time = glfwGetTime();
-    float speed = 0.01f;
-
     process_input(window);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     shader.use(); // shader code
 
-    // change angle with keys a and s
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    {
-      //x_offset -= 0.01f;
-      angle -= 0.01f;
-    }
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    {
-      //x_offset += 0.01f;
-      angle += 0.01f;
-    }
+    // cube matrices
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), 
+                                 glm::vec3(0.0f, 0.0f, 0.0f), 
+                                 glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
 
-    // change position based on angle with keys w and s
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    {
-      x_offset += cos(angle) * speed;
-      y_offset += sin(angle) * speed;
-    }
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    {
-      x_offset += -(cos(angle) * speed);
-      y_offset += -(sin(angle) * speed);
-    }
-
-    // set size
-    float size = 0.5f;
-
-    shader.set_float("xOffset", x_offset); // movement in x axis
-    shader.set_float("yOffset", y_offset); // movement in y axis
-
-    shader.set_float("size", size); 
-
-    shader.set_float("angle", angle);
-
-    shader.set_float("u_time", glfwGetTime()); // color breath animation
+    // set values
+    shader.set_mat4("model", model);
+    shader.set_mat4("view", view);
+    shader.set_mat4("projection", projection);
 
     glBindVertexArray(vao); // use vertex array obj
 
-    glDrawArrays(GL_TRIANGLES, 0, 3); // draw triangle
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
