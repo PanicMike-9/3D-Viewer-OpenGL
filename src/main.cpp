5f80625ae04c8d68a94633fd8f79358f271d9c60
PanicMike-9 <omsaudager990@gmail.com>
2026-06-10 23:50:54 +0530

    Refactor(main) fix formatting and comment style

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,10 @@
 // shader code
 #include "shader.hpp"
 
-// window height and width values
+// window height, width and aspect ratio values
 const float win_width = 800.0f;
 const float win_height = 600.0f;
+const float win_aspect = win_width / win_height;
 
 constexpr const double PI = 3.141592653589793; 
 
@@ -30,17 +31,26 @@ void render()
 // exit window with q or esc keys
 void process_input(GLFWwindow *window)
 {
-  if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-      glfwSetWindowShouldClose(window, true);
+
+    if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
+// unique cube position for each new cube
 std::vector<glm::vec3> cube_position 
 {
-  //          x      y      z
-  glm::vec3( 0.0f,  0.0f,  0.0f),
-  glm::vec3( 1.0f, -1.0f, -1.0f),
-  glm::vec3(-1.0f, -1.0f,  1.0f),
-  glm::vec3( 2.0f,  2.0f,  2.0f),
+    //          x      y      z
+    glm::vec3( 0.0f,  0.0f,  0.0f), // origin as z = 0
+    glm::vec3( 1.0f, -1.0f,  0.0f),
+    glm::vec3(-1.0f, -1.0f,  0.0f),
+    glm::vec3(-1.0f,  1.0f,  0.0f),
+    glm::vec3( 1.0f,  1.0f,  0.0f),
+
+    glm::vec3( 0.0f,  0.0f,  2.0f), // origin as z = 2
+    glm::vec3( 1.0f, -1.0f,  2.0f),
+    glm::vec3(-1.0f, -1.0f,  2.0f),
+    glm::vec3(-1.0f,  1.0f,  2.0f),
+    glm::vec3( 1.0f,  1.0f,  2.0f),
 };
 
 void update_transform_matrices(Shader& shader, float& angle)
@@ -48,20 +58,20 @@ void update_transform_matrices(Shader& shader, float& angle)
     // cube matrices loop
     for(const glm::vec3& pos : cube_position)
     {
-      glm::mat4 model_1 = glm::mat4(1.0f);
-      model_1 = glm::translate(model_1, pos);
+        glm::mat4 model_1 = glm::mat4(1.0f);
+        model_1 = glm::translate(model_1, pos);
 
-      // rotates the cube                             
-      //model_1 = glm::rotate(model_1, angle, glm::vec3(1.0f, 0.0f, 0.0f));
+        // rotates the cube                             
+        //model_1 = glm::rotate(model_1, angle, glm::vec3(1.0f, 0.0f, 0.0f));
 
-      shader.set_mat4("model", model_1);
-      render();
+        shader.set_mat4("model", model_1);
+        render();
     }
 
     glm::mat4 view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), 
                                  glm::vec3(0.0f, 0.0f, 0.0f), 
                                  glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 projection = glm::perspective(glm::radians(90.0f), 800.0f/600.0f, 0.1f, 100.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(90.0f), win_aspect, 0.1f, 100.0f);
 
     // set view and projection
     shader.set_mat4("view", view);
@@ -69,42 +79,44 @@ void update_transform_matrices(Shader& shader, float& angle)
 }
 
 // rotate, increase and decrease speed and angle of the cube vertices
-//void update_input(GLFWwindow *window, float& angle, float& speed)
-//{
-//    // increase speed using left and right arrow keys
-//    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-//    {
-//      speed += 0.01f;
-//
-//      if(speed > 2.5f)
-//      {
-//        speed = 2.5f;
-//      }
-//
-//      std::cout << "Speed: " << std::fixed << std::setprecision(2) << speed << '\n';
-//    }
-//    else if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-//    {
-//      speed -= 0.01f;
-//
-//      if(speed <= 0.0f)
-//      {
-//        speed = 0.0f;
-//      }
-//
-//      std::cout << "Speed: " << std::fixed << std::setprecision(2) << speed << '\n';
-//    }
-//
-//    // increment angle by 0.01 for slower rotation speed
-//    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-//    {
-//      angle += 0.01f * speed;
-//    }
-//    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-//    {
-//      angle += -0.01f * speed;
-//    }
-//}
+/*
+void update_input(GLFWwindow *window, float& angle, float& speed)
+{
+    // increase speed using left and right arrow keys
+    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+      speed += 0.01f;
+
+      if(speed > 2.5f)
+      {
+        speed = 2.5f;
+      }
+
+      std::cout << "Speed: " << std::fixed << std::setprecision(2) << speed << '\n';
+    }
+    else if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    {
+      speed -= 0.01f;
+
+      if(speed <= 0.0f)
+      {
+        speed = 0.0f;
+      }
+
+      std::cout << "Speed: " << std::fixed << std::setprecision(2) << speed << '\n';
+    }
+
+    // increment angle by 0.01 for slower rotation speed
+    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+      angle += 0.01f * speed;
+    }
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+      angle += -0.01f * speed;
+    }
+}
+*/
 
 // simple origin orbit camera logic
 void orbit_camera(Shader& shader)
@@ -114,10 +126,8 @@ void orbit_camera(Shader& shader)
     float cam_x = sin(glfwGetTime()) * radius;
     float cam_z = cos(glfwGetTime()) * radius;
 
-    float aspect = win_width/win_height;
-
     // camera view and projection
-    glm::mat4 camera_projection = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 100.0f);
+    glm::mat4 camera_projection = glm::perspective(glm::radians(45.0f), win_aspect, 0.1f, 100.0f);
     glm::mat4 camera_view = glm::lookAt(glm::vec3(cam_x, 0.0f, cam_z), //position
                                         glm::vec3(0.0f, 0.0f, 0.0f),   // target
                                         glm::vec3(0.0f, 1.0f, 0.0f));  // up vector
@@ -130,143 +140,144 @@ void orbit_camera(Shader& shader)
 // main
 int main()
 {
-  glfwInit();
 
-  // declare window name and size
-  GLFWwindow* window = glfwCreateWindow(win_width, win_height, "OpenGL Cube", NULL, NULL);
+    glfwInit();
 
-  if(!window)
-  {
-    std::cerr << "Could not create window!\n";
+    // declare window name and size
+    GLFWwindow* window = glfwCreateWindow(win_width, win_height, "OpenGL Cube", NULL, NULL);
+
+    if(!window)
+    {
+        std::cerr << "Could not create window!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << "Window created!\n";
+
+    // create window
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Could not initialize GLAD\n";
+        return -1;
+    }
+
+    std::cout << "GLAD Loaded\n";
+
+    // window background color
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+
+    float cube_vertices[]
+    {
+    //    x      y     z       r     g     b
+        -0.5f, -0.5f, 0.5f,   1.0f, 0.5f, 1.0f, 
+         0.5f, -0.5f, 0.5f,   0.8f, 0.2f, 1.0f, 
+         0.5f,  0.5f, 0.5f,   0.5f, 0.6f, 1.0f,
+        -0.5f,  0.5f, 0.5f,   0.7f, 0.4f, 1.0f,
+
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.5f,
+         0.5f, -0.5f, -0.5f,  0.5f, 1.0f, 0.6f,
+         0.5f,  0.5f, -0.5f,  0.8f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.4f, 1.0f, 0.6f,
+    };
+
+    GLuint cube_indices[]
+    {
+        // front face
+        0, 1, 2,
+        0, 2, 3,
+
+        // back face
+        4, 5, 6,
+        4, 6, 7,
+
+        // right face
+        1, 5, 6,
+        1, 6, 2,
+
+        // left face
+        0, 4, 7,
+        0, 7, 3,
+
+        // top face
+        2, 3, 7,
+        2, 7, 6,
+
+        // bottom face
+        0, 1, 5,
+        0, 5, 4,
+    };
+
+    // vertex buffer object (VBO)
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices) * sizeof(float), cube_vertices, GL_STATIC_DRAW);
+
+    // vertex array object(VAO)
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // element buffer objec (EBO) for cube
+    GLuint ebo = 0;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+
+    // unbind
+    glBindVertexArray(0);
+
+    // shader code files
+    Shader shader("shaders/v_shader.vert", "shaders/f_shader.frag");
+
+    // enable depth test to view in 3d
+    glEnable(GL_DEPTH_TEST);
+
+    // angle for rotation
+    float angle = 0.0f;
+
+    // speed for rotation
+    float speed = 0.0f;
+
+    // main window loop
+    while(!glfwWindowShouldClose(window))
+    {
+        process_input(window);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.use(); // shader code
+
+        glBindVertexArray(vao); // use vertex array obj
+
+        update_transform_matrices(shader, angle); // cube view and rotation
+
+        orbit_camera(shader);
+
+        //update_input(window, angle, speed); // cube movement logic
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // destroy window
+    glfwDestroyWindow(window);
+    std::cout << "glfwDestroyWindow(window) executed\n";
+
+    // terminate glfw
     glfwTerminate();
-    return -1;
-  }
+    std::cout << "glfwTerminate() executed\n";
 
-  std::cout << "Window created!\n";
-
-  // create window
-  glfwMakeContextCurrent(window);
-
-  if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-  {
-    std::cerr << "Could not initialize GLAD\n";
-    return -1;
-  }
-
-  std::cout << "GLAD Loaded\n";
-
-  // window background color
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
-
-  float cube_vertices[]
-  {
-//   x      y     z        r     g     b
-    -0.5f, -0.5f, 0.5f,   1.0f, 0.5f, 1.0f, 
-     0.5f, -0.5f, 0.5f,   0.8f, 0.2f, 1.0f, 
-     0.5f,  0.5f, 0.5f,   0.5f, 0.6f, 1.0f,
-    -0.5f,  0.5f, 0.5f,   0.7f, 0.4f, 1.0f,
-
-    -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.5f,
-     0.5f, -0.5f, -0.5f,  0.5f, 1.0f, 0.6f,
-     0.5f,  0.5f, -0.5f,  0.8f, 1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  0.4f, 1.0f, 0.6f,
-  };
-
-  GLuint cube_indices[]
-  {
-      // front face
-      0, 1, 2,
-      0, 2, 3,
-
-      // back face
-      4, 5, 6,
-      4, 6, 7,
-
-      // right face
-      1, 5, 6,
-      1, 6, 2,
-
-      // left face
-      0, 4, 7,
-      0, 7, 3,
-
-      // top face
-      2, 3, 7,
-      2, 7, 6,
-
-      // bottom face
-      0, 1, 5,
-      0, 5, 4,
-  };
-
-  // vertex buffer object (VBO)
-  GLuint vbo = 0;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices) * sizeof(float), cube_vertices, GL_STATIC_DRAW);
-
-  // vertex array object(VAO)
-  GLuint vao = 0;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
-  // position
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
-
-  // color
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  // element buffer objec (EBO) for cube
-  GLuint ebo = 0;
-  glGenBuffers(1, &ebo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
-
-  // unbind
-  glBindVertexArray(0);
-
-  // shader code files
-  Shader shader("shaders/v_shader.vert", "shaders/f_shader.frag");
-
-  // enable depth test to view in 3d
-  glEnable(GL_DEPTH_TEST);
-
-  // angle for rotation
-  float angle = 0.0f;
-
-  // speed for rotation
-  float speed = 0.0f;
-
-  // main window loop
-  while(!glfwWindowShouldClose(window))
-  {
-    process_input(window);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    shader.use(); // shader code
-
-    glBindVertexArray(vao); // use vertex array obj
-
-    update_transform_matrices(shader, angle); // cube view and rotation
-
-    orbit_camera(shader);
-
-    //update_input(window, angle, speed); // cube movement logic
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  // destroy window
-  glfwDestroyWindow(window);
-  std::cout << "glfwDestroyWindow(window) executed\n";
-
-  // terminate glfw
-  glfwTerminate();
-  std::cout << "glfwTerminate() executed\n";
-
-  return 0;
+    return 0;
 }
