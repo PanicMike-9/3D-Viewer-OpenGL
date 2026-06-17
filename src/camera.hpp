ba0d555f6a0da3584d833b86e1e161297c830633
PanicMike-9 <omsaudager990@gmail.com>
2026-06-17 21:59:41 +0530

    Refactor(camera): add comments and add required variables

--- src/camera.hpp
+++ src/camera.hpp
@@ -13,13 +13,17 @@ struct Camera
     glm::vec3 m_front    = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 m_up       = glm::vec3(0.0f, 1.0f, 0.0f);
     glm::vec3 m_right;
+    glm::vec3 m_world_up;
     
+    // camera options
     float m_speed = 5.5f;
     float m_fov   = 45.0f;
 
+    // for consistent timing 
     float m_delta_time = 0.0f;
     float m_last_frame = 0.0f;
 
+    // euler angles
     float m_yaw        = -90.0f;
     float m_pitch      = 0.0f;
     bool m_first_mouse = true;
