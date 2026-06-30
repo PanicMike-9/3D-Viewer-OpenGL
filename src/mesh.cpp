d858065c28e8116493ed834279fba394ecee61cc
PanicMike-9 <omsaudager990@gmail.com>
2026-06-30 19:44:12 +0530

    Feat: add setup_mesh() in mesh.cpp

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,5 +1,6 @@
 #include "mesh.hpp"
 
+// constructor
 Mesh::Mesh
 (
     std::vector<Vertex> vertices, 
@@ -8,4 +9,30 @@ Mesh::Mesh
 ) : vertices{vertices}, indices{indices}, textures{textures}
 {
     setup_mesh();
+}
+
+void Mesh::setup_mesh()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER , EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tex_coords));
+
+    glBindVertexArray(0);
 }
