43ff7e9c525f9cdb032876f51d0e92ebc69af993
PanicMike-9 <omsaudager990@gmail.com>
2026-07-01 22:13:40 +0530

    Feat: add draw function

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -7,6 +7,7 @@ Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std:
     setup_mesh();
 }
 
+// set up the mesh's vertex attributes
 void Mesh::setup_mesh()
 {
     // set up VAO, VBO, EBO
@@ -38,5 +39,37 @@ void Mesh::setup_mesh()
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tex_coords));
 
     // unbind the VAO
+    glBindVertexArray(0);
+}
+
+// render textures and geometry
+void Mesh::draw(Shader &shader)
+{
+    unsigned int diffuse_nr  = 1;
+    unsigned int specular_nr = 1;
+
+    // bind textures
+    for(unsigned int i = 0; i < textures.size(); ++i)
+    {
+        // activate the correct texture unit
+        glActiveTexture(GL_TEXTURE0 + i);
+
+        std::string number;
+        std::string name = textures[i].type;
+
+        if(name == "texture_diffuse")
+            number = std::to_string(diffuse_nr++);
+
+        else if(name == "texture_specular")
+            number = std::to_string(specular_nr++);
+        
+        shader.set_int(("material", name + number).c_str(), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+
+    glActiveTexture(GL_TEXTURE0);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 }
