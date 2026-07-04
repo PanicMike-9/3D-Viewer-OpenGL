0cb2c1e93cc2a9b93930efa8b78bc2ff993c3ecb
PanicMike-9 <omsaudager990@gmail.com>
2026-07-04 20:08:28 +0530

    Feat: add draw function

--- src/model.cpp
+++ src/model.cpp
@@ -5,3 +5,11 @@ Model::Model(char *path)
 {
     load_model(path);
 }
+
+void Model::draw(Shader &shader)
+{
+    for(unsigned int i = 0; i < meshes.size(); i++)
+    {
+        meshes[i].draw(shader);
+    }
+}
