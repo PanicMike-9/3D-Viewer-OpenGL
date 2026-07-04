6ca8de0c4791200e84f9c4c1c3608123107b2116
PanicMike-9 <omsaudager990@gmail.com>
2026-07-04 20:49:29 +0530

    Feat: add load_model(), add comments for clarification

--- src/model.cpp
+++ src/model.cpp
@@ -6,10 +6,28 @@ Model::Model(char *path)
     load_model(path);
 }
 
+// Draw meshes
 void Model::draw(Shader &shader)
 {
     for(unsigned int i = 0; i < meshes.size(); i++)
     {
-        meshes[i].draw(shader);
+        meshes[i].draw(shader); // calls draw() from Mesh class
     }
+}
+
+// load models and handle model loading error
+void Model::load_model(std::string path)
+{
+    Assimp::Importer import;
+    const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+    if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+    {
+        std::cout << "ERROR::ASSIMP:: " << import.GetErrorString() << '\n';
+        return;
+    }
+
+    directory = path.substr(0, path.find_last_of('/'));
+
+    process_node(scene->mRootNode, scene);
 }
