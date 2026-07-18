15004dbbedb05fb5dc874d16d5d02b969b6a71a2
PanicMike-9 <omsaudager990@gmail.com>
2026-07-18 21:27:05 +0530

    Feat(model): add textures_loaded vector to check and verify already loaded textures

--- src/model.hpp
+++ src/model.hpp
@@ -24,6 +24,7 @@ class Model
     
     private:
         std::vector<Mesh> meshes;
+        std::vector<Texture> textures_loaded;
         std::string directory;
 
         void load_model(std::string path);
