e32328784ec8d09d25de7d8d1b1af7c04bcbd1fe
PanicMike-9 <omsaudager990@gmail.com>
2026-07-29 23:43:01 +0530

    Update(shader): define set_vec3 function

--- src/shader.cpp
+++ src/shader.cpp
@@ -121,3 +121,8 @@ void Shader::set_mat4(const std::string &name, const glm::mat4 &mat) const
 {
     glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
 }
+
+void Shader::set_vec3(const std::string& name, const glm::vec3& vec) const
+{
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), vec.x, vec.y, vec.z);
+}
