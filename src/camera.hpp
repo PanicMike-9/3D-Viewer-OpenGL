7c274a34bbc914ff7645d8ff54872b2c887de8ba
PanicMike-9 <omsaudager990@gmail.com>
2026-09-06 16:28:42 +0530

    refactor(camera): use initializer list for variables

--- src/camera.hpp
+++ src/camera.hpp
@@ -23,22 +23,23 @@ class Camera
 {
     public:
         // camera motion position
-        glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f); 
-        glm::vec3 front =    glm::vec3(0.0f, 0.0f, -1.0f);
-        glm::vec3 up =       glm::vec3(0.0f, 1.0f, 0.0f);
-        glm::vec3 right =    glm::vec3(1.0f, 0.0f, 0.0f);
-        glm::vec3 world_up = glm::vec3(0.0f, 1.0f, 0.0f);
+        glm::vec3 position  {glm::vec3(0.0f, 0.0f, 3.0f)}; 
+        glm::vec3 front     {glm::vec3(0.0f, 0.0f, -1.0f)};
+        glm::vec3 up        {glm::vec3(0.0f, 1.0f, 0.0f)};
+        glm::vec3 right     {glm::vec3(1.0f, 0.0f, 0.0f)};
+        glm::vec3 world_up  {glm::vec3(0.0f, 1.0f, 0.0f)};
         
         // camera details
-        float speed = 5.5f;
-        float fov =   45.0f;
+        float speed {5.5f};
+        float fov {45.0f};
 
         // euler angles
-        float yaw =        -90.0f;
-        float pitch =       0.0f;
-        float sensitivity = 0.1f;
+        float yaw         {-90.0f};
+        float pitch       { 0.0f};
+        float sensitivity { 0.1f};
 
-        // constructor
+        CameraMode mode {CameraMode::FLY}; // default mode
+
         Camera();
 
         // matrix functions
