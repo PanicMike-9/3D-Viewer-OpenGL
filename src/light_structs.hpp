b25c56411236b1af261f764312af9f326a159996
PanicMike-9 <omsaudager990@gmail.com>
2026-09-07 22:46:41 +0530

    refactor(light_structs): variable alignment

--- src/light_structs.hpp
+++ src/light_structs.hpp
@@ -19,24 +19,24 @@ struct PointLight
     glm::vec3 diffuse  {0.8f, 0.8f, 0.8f};
     glm::vec3 specular {1.0f, 1.0f, 1.0f};
 
-    float constant {1.0f};
-    float linear {0.09f};
+    float constant  {1.0f};
+    float linear    {0.09f};
     float quadratic {0.032f};
 };
 
 struct SpotLight
 {
-    glm::vec3 position {0.0f, 0.0f, 0.0f};
+    glm::vec3 position  {0.0f, 0.0f, 0.0f};
     glm::vec3 direction {0.0f, 0.0f, -1.0f};
 
     glm::vec3 ambient  {0.0f, 0.0f, 0.0f};
     glm::vec3 diffuse  {1.0f, 1.0f, 1.0f};
     glm::vec3 specular {1.0f, 1.0f, 1.0f};
     
-    float cut_off {12.5f};
+    float cut_off       {12.5f};
     float outer_cut_off {15.0f};
 
-    float constant {1.0f};
-    float linear {0.09f};
+    float constant  {1.0f};
+    float linear    {0.09f};
     float quadratic {0.032f};
 };
