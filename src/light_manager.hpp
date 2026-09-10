51c233cdcdea997663720b0e53d566b5d7a3d267
PanicMike-9 <omsaudager990@gmail.com>
2026-09-10 19:35:38 +0530

    feat(light): add and set directional light

--- src/light_manager.hpp
+++ src/light_manager.hpp
@@ -28,5 +28,17 @@ class LightManager
 
         void update_shader_uniforms(Shader& shader, [[maybe_unused]] const Camera& camera)
         {
+            shader.use();
+
+            constexpr glm::vec3 light_color {1.0f, 1.0f, 1.0f};
+
+            constexpr glm::vec3 ambient_color  {light_color * glm::vec3(0.2f)}; // shadow brightness
+            constexpr glm::vec3 diffuse_color  {light_color * glm::vec3(0.8f)}; // direct surface light 
+            constexpr glm::vec3 specular_color {light_color * glm::vec3(1.0f)}; // brightness of shine
+
+            shader.set_vec3("dir_light.direction", dir_light.direction);
+            shader.set_vec3("dir_light.ambient",   dir_light.ambient);
+            shader.set_vec3("dir_light.diffuse",   dir_light.diffuse);
+            shader.set_vec3("dir_light.specular",  dir_light.specular);
         }
 };
