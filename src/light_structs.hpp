43d69c61d8bc1b7292a0ac7a95fb87cd87a28453
PanicMike-9 <omsaudager990@gmail.com>
2026-09-13 01:44:08 +0530

    fix(light): increase directional light specular to 1, diffuse to 0.8 and ambient to 0.25, for brighter directional light

--- src/light_structs.hpp
+++ src/light_structs.hpp
@@ -6,9 +6,9 @@ struct DirectLight
 {
     glm::vec3 direction {-0.2f, -1.0f, -0.3f};
 
-    glm::vec3 ambient  {0.05f, 0.05f, 0.05f};
-    glm::vec3 diffuse  {0.4f, 0.4f, 0.4f};
-    glm::vec3 specular {0.5f, 0.5f, 0.5f};
+    glm::vec3 ambient  {0.25f, 0.25f, 0.25f};
+    glm::vec3 diffuse  {0.8f, 0.8f, 0.8f};
+    glm::vec3 specular {1.0f, 1.0f, 1.0f};
 };
 
 struct PointLight
