929033df1d905378b66f6a508b647fc141287da4
PanicMike-9 <omsaudager990@gmail.com>
2026-09-20 00:24:22 +0530

    impr(light): use the spot and point light data for update_shader_uniforms

--- src/light_manager.hpp
+++ src/light_manager.hpp
@@ -177,47 +177,49 @@ class LightManager
             shader.set_vec3("dir_light.diffuse",   light_color * dir_light.diffuse);
             shader.set_vec3("dir_light.specular",  light_color * dir_light.specular);
 
-            #if 0
+            #if 1
             // point lights
             shader.set_int("num_point_lights", static_cast<int>(point_lights.size()));
 
             for (std::size_t i {0}; i < point_lights.size(); ++i)
             {
+                const PointLight& light = point_lights[i].pl_data;
                 std::string base = "point_lights[" + std::to_string(i) + "].";
 
-                shader.set_vec3(base + "position", point_lights[i].position);
+                shader.set_vec3(base + "position", light.position);
 
-                shader.set_vec3(base + "ambient",  point_lights[i].ambient); 
-                shader.set_vec3(base + "diffuse",  point_lights[i].diffuse);
-                shader.set_vec3(base + "specular", point_lights[i].specular);
+                shader.set_vec3(base + "ambient",  light.ambient); 
+                shader.set_vec3(base + "diffuse",  light.diffuse);
+                shader.set_vec3(base + "specular", light.specular);
 
-                shader.set_float(base + "constant",  point_lights[i].constant);
-                shader.set_float(base + "linear",    point_lights[i].linear);
-                shader.set_float(base + "quadratic", point_lights[i].quadratic);
+                shader.set_float(base + "constant",  light.constant);
+                shader.set_float(base + "linear",    light.linear);
+                shader.set_float(base + "quadratic", light.quadratic);
             }
             #endif
 
-            #if 0
+            #if 1
             // spot light
             shader.set_int("num_spot_lights", static_cast<int>(spot_lights.size()));
 
             for (std::size_t i {0}; i < spot_lights.size(); ++i)
             {
+                const SpotLight& light = spot_lights[i].sl_data;
                 std::string base = "spot_lights[" + std::to_string(i) + "].";
 
-                shader.set_vec3(base + "position", spot_lights[i].position);
-                shader.set_vec3(base + "direction", spot_lights[i].direction);
+                shader.set_vec3(base + "position",  light.position);
+                shader.set_vec3(base + "direction", light.direction);
 
-                shader.set_vec3(base + "ambient",  spot_lights[i].ambient); 
-                shader.set_vec3(base + "diffuse",  spot_lights[i].diffuse);
-                shader.set_vec3(base + "specular", spot_lights[i].specular);
+                shader.set_vec3(base + "ambient",  light.ambient); 
+                shader.set_vec3(base + "diffuse",  light.diffuse);
+                shader.set_vec3(base + "specular", light.specular);
 
-                shader.set_float(base + "constant",  spot_lights[i].constant);
-                shader.set_float(base + "linear",    spot_lights[i].linear);
-                shader.set_float(base + "quadratic", spot_lights[i].quadratic);
+                shader.set_float(base + "constant",  light.constant);
+                shader.set_float(base + "linear",    light.linear);
+                shader.set_float(base + "quadratic", light.quadratic);
 
-                shader.set_float(base + "cut_off",  glm::cos(glm::radians(spot_lights[i].cut_off))); 
-                shader.set_float(base + "outer_cut_off", glm::cos(glm::radians(spot_lights[i].outer_cut_off))); 
+                shader.set_float(base + "cut_off",  glm::cos(glm::radians(light.cut_off))); 
+                shader.set_float(base + "outer_cut_off", glm::cos(glm::radians(light.outer_cut_off))); 
             }
             #endif
         }
